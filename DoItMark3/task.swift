d8dff27d41997ce83de2df4a29a1bdd69d3ca6c4
Michael Ruddy <mrud2001@outlook.com>
2017-07-24 17:46:05 -0700

    working

---
+++ DoItMark3/task.swift
@@ -0,0 +1,13 @@
+//
+//  task.swift
+//  DoItMark3
+//
+//  Created by Michael Ruddy on 7/24/17.
+//  Copyright © 2017 Michael Ruddy. All rights reserved.
+//
+
+import Foundation
+class Task {
+    var name = ""
+    var important = false
+}
